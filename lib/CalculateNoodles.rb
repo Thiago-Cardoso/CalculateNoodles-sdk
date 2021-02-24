0180eb53165b23f3e07e9adf4fb200f3bcc48730
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-02-23 22:53:45 -0300

    structure

---
+++ lib/CalculateNoodles.rb
@@ -0,0 +1,6 @@
+require "CalculateNoodles/version"
+
+module CalculateNoodles
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/CalculateNoodles/version.rb
@@ -0,0 +1,3 @@
+module CalculateNoodles
+  VERSION = "0.1.0"
+end
